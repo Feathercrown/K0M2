9e27ac5a2eced3bb68bf48885c33a69aa33d5d75
Cubic-Zirconia <crystaltheandroid@gmail.com>
2017-12-17 14:24:53 -0500

    Reload is FIXED

--- shell/commands/reload.js
+++ shell/commands/reload.js
@@ -1,6 +1,6 @@
 const Shell = require('../../klari0m2.js');
 exports.run = (client, message, args, sudo) => {
-    if(!args) return Shell.log("Must provide a command name to reload.",1);
+    if(args==[]) return Shell.log("Must provide a command name to reload.",1);
     // the path is relative to the *current folder*, so just ./filename.js
     else {
         delete require.cache[require.resolve(`./${args[0]}.js`)];
