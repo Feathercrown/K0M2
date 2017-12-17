1699c2a46c17eca310d6a4a2394c9bc1dc7cb479
Cubic-Zirconia <crystaltheandroid@gmail.com>
2017-12-17 14:15:14 -0500

    oops

--- shell/commands/listchannels.js
+++ shell/commands/listchannels.js
@@ -3,6 +3,6 @@ exports.run = (client, message, args, sudo) => {
     var channels = Array.from(Array.from(client.guilds)[0][1].channels);
     for(var i=0;i<channels.length;i++){
         var curchan = channels[i][1];
-        Shell.log("Name: ${curchan.name}\nType: ${curchan.type}"+curchan.topic==""?"":curchan.topic,1);
+        Shell.log(`Name: ${curchan.name}\nType: ${curchan.type}`+curchan.topic==""?"":curchan.topic,1);
     }
 };
