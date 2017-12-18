28891d3a555832fea70c7ddf74a5a3d86c3f8e0c
Cubic-Zirconia <crystaltheandroid@gmail.com>
2017-12-17 21:08:52 -0500

    Added extra bits to Audio

--- shell/commands/audio.js
+++ shell/commands/audio.js
@@ -1,18 +1,25 @@
-const Shell = require('../../klari0m2.js');
-const config = require('../../config.json');
-const ytdl = require('ytdl-core');
+const 
+    Shell = require('../../klari0m2.js'),
+    config = require('../../config.json'),
+    ytdl = require('ytdl-core');
+var idle = false;
 exports.run = (client, message, args, sudo, conn) => {
+
     let url = ytdl(args[1]);
     if(args[0]=='play'){
         //Play command recieved
         conn.playFile('./shell/assets/playing_audio.mp3')
             .once("start", start => {
+                idle = false;
                 Shell.log('Playing audio file...',1);
             })
             .once("end", end => {
                 conn.playStream(url)
-                .once("end", end => {
-                    Shell.log('Audio stream over.',1);
+                    .once("end", end => {
+                    conn.playFile('./shell/assets/audio_over.mp3')
+                        .once("start", start => {
+                            Shell.log('Audio stream over.',1);
+                    });
                 });
             });   
     }
