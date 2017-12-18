19a6d78d7c9d2d8dd5bd0b35bf24735a3af34e35
Cubic-Zirconia <crystaltheandroid@gmail.com>
2017-12-18 18:26:37 -0500

    Update reload and test

--- shell/commands/reload.js
+++ shell/commands/reload.js
@@ -1,8 +1,11 @@
 const Shell = require('../../klari0m2.js');
 exports.run = (client, message, args, sudo) => {
-    if(args==[]) return Shell.log("Must provide a command name to reload.",1);
-    // the path is relative to the *current folder*, so just ./filename.js
-    else {
+    console.log(args);
+    if(args===[]){
+        Shell.log("Must provide a command name to reload.",1);
+        return
+    } else {
+        // the path is relative to the *current folder*, so just ./filename.js
         delete require.cache[require.resolve(`./${args[0]}.js`)];
         Shell.log(`The "${args[0]}" module has been reloaded.`,1);
     }
--- shell/commands/test.js
+++ shell/commands/test.js
@@ -2,7 +2,7 @@ const Shell = require('../../klari0m2.js');
 exports.run = (client, message, args, sudo, conn) => {
     if(args[0]=='voice'){
         conn.playFile('./shell/assets/test.mp3');
-        Shell.log('Playing audio file. If you don\'t hear this message, something probably isn\'t working.\nBut you\'re hearing it now, so good job.',1);
+        Shell.log('Playing audio file. If you don\'t hear this message, something probably isn\'t working.',1);
     } else if(sudo){
         Shell.log(`Everything's in working order, master ${message.author.username}.`,1);
     } else {
