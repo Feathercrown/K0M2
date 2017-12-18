6372a9ed65474a33b1ec264a795f892b72dcf9cb
Cubic-Zirconia <crystaltheandroid@gmail.com>
2017-12-17 20:29:21 -0500

    sandbox has been changed to audio

--- shell/commands/audio.js
+++ shell/commands/audio.js
@@ -5,8 +5,10 @@ exports.run = (client, message, args, sudo, conn) => {
     let url = ytdl(args[1]);
     if(args[0]=='play'){
         //Play command recieved
-        Shell.log('Playing audio file...',1);
         conn.playFile('./shell/assets/playing_audio.mp3')
+            .once("start", start => {
+                Shell.log('Playing audio file...',1);
+            })
             .once("end", end => {
                 conn.playStream(url)
                 .once("end", end => {
