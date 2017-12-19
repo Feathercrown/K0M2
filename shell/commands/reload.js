413344bfef18e249b118f0bbcfaad1284df68838
Klari <crystaltheandroid@gmail.com>
2017-12-19 08:55:09 -0500

    YOU SCRUB!

--- shell/commands/reload.js
+++ shell/commands/reload.js
@@ -1,5 +1,12 @@
 const Shell = require('../../klari0m2.js');
 exports.run = (client, message, args, sudo) => {
+    if(!sudo){
+        Shell.respond([
+            `${message.author.username}, you don't have permission to use this command!`,
+            `NO, ${message.author.username}! I don't listen to wankers not on "the list."`
+        ]);
+        return;
+    }
     if(!args[0]){
         Shell.respond([
             `${message.author.username}, this command takes arguments, and you didn't give any.`,
